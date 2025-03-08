c055e3213d8118fd69c4e693c434ec7de18007f4
andercrzz <98536868+andercrzz@users.noreply.github.com>
2025-03-08 17:26:32 +0100

    carrito y precio total implementados

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:robot/model/category_model.dart';
 import 'package:robot/model/product_model.dart';
-import 'package:robot/pages/cart.dart';
 import 'package:robot/service/cart.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:robot/pages/product_detail.dart';
 import 'package:robot/service/widget_support.dart';
 
 class Home extends StatefulWidget {
@@ -41,106 +39,118 @@ class _HomeState extends State<Home> {
     setState(() {});
   }
 
+  double getTotalPrice() {
+    double result = Cart.items.fold(0, (total, current) => total + double.parse(current.precio));
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => CartPage()),
-              );
-            },
+      body: Row(
+        children: [
+          Container(
+            width: 250, // Ancho del menú lateral
+            color: Color.fromARGB(168, 56, 39, 23),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(168, 56, 39, 23),
+                  ),
+                  child: Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            "images/logo.png",
+                            height: 50,
+                            width: 110,
+                            fit: BoxFit.contain),
+                          Text("Order your favorite food",
+                            style: AppWidget.SimpleTextFieldStyle()
+                          ),
+                      ]),
+                    ],
+                  ),
+                ),
+                ...categories.map((category) {
+                  return ListTile(
+                    title: Text(category.nombre, style: TextStyle(color: Colors.white)),
+                    onTap: () {
+                      getProducts(category.nombre);
+                    },
+                  );
+                }).toList(),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.only(left: 20.0, top: 40.0),
+              child: Column(
+                children: [
+                  Container(
+                    height: 70,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: categories.length,
+                      itemBuilder: (context, index){
+                        return CategoryTile(
+                          categories[index].nombre,
+                          //categories[index].image,
+                          "images/burger.png",
+                          index.toString(),
+                          onTap: () {
+                            getProducts(categories[index].nombre);
+                          },
+                        );
+                      }),
+                  ),
+                  SizedBox(height: 20.0),
+                  Expanded(
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.75,
+                      ),
+                      itemCount: products.length,
+                      itemBuilder: (context, index) {
+                        return ProductTile(
+                          product: products[index],
+                          onAddToCart: () {
+                            setState(() {});
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(
+                    'Total: \$${getTotalPrice().toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => CartPage(onCartUpdated: () {
+                          setState(() {});
+                        })),
+                      );
+                    },
+                    child: Text('Terminar compra'),
+                  ),
+                ]
+              )
+            ),
           ),
         ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-              ),
-              child: Text(
-                'Categorías',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ...categories.map((category) {
-              return ListTile(
-                title: Text(category.nombre),
-                onTap: () {
-                  Navigator.pop(context); // Cierra el drawer
-                  getProducts(category.nombre);
-                },
-              );
-            }).toList(),
-          ],
-        ),
-      ),
-      body: Container(
-        margin: EdgeInsets.only(left: 20.0, top: 40.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.asset(
-                      "images/logo.png",
-                      height: 50,
-                      width: 110,
-                      fit: BoxFit.contain),
-                    Text("Order your favorite food",
-                      style: AppWidget.SimpleTextFieldStyle()
-                    ),
-                ]),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Container(
-              height: 70,
-              child: ListView.builder(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: categories.length,
-                itemBuilder: (context, index){
-                  return CategoryTile(
-                    categories[index].nombre,
-                    //categories[index].image,
-                    "images/burger.png",
-                    index.toString(),
-                    onTap: () {
-                      getProducts(categories[index].nombre);
-                    },
-                  );
-                }),
-            ),
-            SizedBox(height: 20.0),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 0.75,
-                ),
-                itemCount: products.length,
-                itemBuilder: (context, index) {
-                  return ProductTile(product: products[index]);
-                },
-              ),
-            ),
-          ]
-        )
-      )
     );
   }
 
@@ -190,8 +200,9 @@ class _HomeState extends State<Home> {
 
 class ProductTile extends StatelessWidget {
   final ProductModel product;
+  final VoidCallback onAddToCart;
 
-  const ProductTile({required this.product});
+  const ProductTile({required this.product, required this.onAddToCart});
 
   @override
   Widget build(BuildContext context) {
@@ -232,11 +243,97 @@ class ProductTile extends StatelessWidget {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(content: Text('${product.nombre} añadido a la cesta')),
               );
+              onAddToCart();
             },
             child: Text('Añadir a la cesta'),
           ),
         ],
       ),
     );
+  }
+}
+
+class CartPage extends StatefulWidget {
+  final VoidCallback onCartUpdated;
+
+  const CartPage({required this.onCartUpdated});
+
+  @override
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mi Carrito'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: Cart.items.length,
+              itemBuilder: (context, index) {
+                final product = Cart.items[index];
+                return ListTile(
+                  leading: Image.network(
+                    product.imagen, // Asegúrate de tener una URL de imagen en tu modelo de producto
+                    height: 50,
+                    width: 50,
+                    fit: BoxFit.cover,
+                  ),
+                  title: Text(product.nombre),
+                  subtitle: Text('Precio: \$${product.precio}'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.remove_circle),
+                    onPressed: () {
+                      setState(() {
+                        Cart.removeItem(product);
+                      });
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('${product.nombre} eliminado de la cesta')),
+                      );
+                      widget.onCartUpdated();
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ElevatedButton(
+              onPressed: () async {
+                await _placeOrder();
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Pedido realizado con éxito')),
+                );
+                setState(() {
+                  Cart.clear();
+                });
+                widget.onCartUpdated();
+                Navigator.pop(context);
+              },
+              child: Text('Pagar'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _placeOrder() async {
+    final FirebaseFirestore firestore = FirebaseFirestore.instance;
+    final CollectionReference orders = firestore.collection('pedidos');
+
+    final List<Map<String, dynamic>> orderItems = Cart.items.map((product) {
+      return product.toMap();
+    }).toList();
+
+    await orders.add({
+      'items': orderItems,
+      'timestamp': FieldValue.serverTimestamp(),
+    });
   }
 }
